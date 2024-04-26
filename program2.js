a02954fd2b4ccfa42fe344efcbbd22945718e54f
ismailbohra <ismailbohra99@gmail.com>
2024-04-26 12:34:37 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,7 +2,13 @@ function longestSubstring(s) {
 
 
     // Implementation of longestSubstring function
-    let longest
+    let longest=0;
+    let seen={};
+    let start=0;
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
 }
 
 module.exports = { longestSubstring };

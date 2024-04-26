f9e600377fa7076e57e2fc0c5787fcbdf266733a
ismailbohra <ismailbohra99@gmail.com>
2024-04-26 12:21:09 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,7 +4,7 @@ function smallestMissingPositiveInteger(nums) {
     // Implement the function smallest_missing_positive_integer
 
     nums.sort((a,b)=>a-b);
-    
+    let smallestpositive =1
   
   }
   

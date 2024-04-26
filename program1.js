c77f05715d5eaaf0958e1e4b5f42b1301c7fde3a
ismailbohra <ismailbohra99@gmail.com>
2024-04-26 12:27:19 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -13,6 +13,6 @@ function smallestMissingPositiveInteger(nums) {
   }
   return smallestpositive
 }
-smallestMissingPositiveInteger()
+smallestMissingPositiveInteger([3,4,1,-1])
 module.exports = smallestMissingPositiveInteger;
 
